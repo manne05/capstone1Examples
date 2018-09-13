e5ffee47f196b6cfbfa89e3d7cc104c7682b518b
manne05 <manfred@m-fromm.de>
2018-09-13 22:50:50 +0200

    Initial version

---
+++ src/de/m/fromm/de/capstone1/Main.java
@@ -0,0 +1,13 @@
+package de.m.fromm.de.capstone1;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git World!");
+	}
+
+}
